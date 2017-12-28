49be05556fde22e6a1906b6d16ec34e85da4592e
binhudakhalid <khalid.bin.huda@hotmail.com>
2017-12-29 01:35:07 +0500

    Updated

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+      $("#info").typed({
+        strings: ["Graduated from University oF karachi", "Programmer", "Machine Learning "],
+        typeSpeed: 30,
+		backDelay: 500,
+		loop: true,
+		showCursor: true,
+		cursorChar: '_'
+      });
+  });
